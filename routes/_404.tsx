95f75098b47a0659af865fdc10e3f390afe9b66b
William Spongberg <william@spongberg.dev>
2025-02-24 02:19:43 +1100

    Enabled footer in 404 page

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -9,7 +9,7 @@ export default function Error404() {
       <Head>
         <title>404 - Page not found</title>
       </Head>
-      <Layout.Page disableFooter>
+      <Layout.Page>
         <Layout.Element>
           <Text.Title>404 - Page not found</Text.Title>
           <br />
